c526db42868a22d7fbf4794ddcf712ecc8a970d5
Kurt Kühnert <kuehnert.kurt95@gmail.com>
2021-10-27 14:15:42 +0200

    compiles on linux

---
+++ cmake-build-debug/CMakeFiles/crossforge.dir/DependInfo.cmake
@@ -0,0 +1,105 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/kurt/Documents/Code/C++/CrossForge/thirdparty/glad/src/glad.c" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/thirdparty/glad/src/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "USE_SYSFS_GPIO"
+  "crossforge_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../thirdparty/glad/include"
+  "../thirdparty/eigen-3.4.0"
+  "/usr/include/opencv4"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/AssetIO/AssimpMeshIO.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/AssetIO/AssimpMeshIO.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/AssetIO/File.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/AssetIO/File.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/AssetIO/I2DImageIO.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/AssetIO/I2DImageIO.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/AssetIO/I3DMeshIO.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/AssetIO/I3DMeshIO.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/AssetIO/OpenCVImageIO.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/AssetIO/OpenCVImageIO.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/AssetIO/SAssetIO.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/AssetIO/SAssetIO.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Core/CForgeObject.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Core/CForgeObject.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Core/CoreUtility.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Core/CoreUtility.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Core/CrossForgeException.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Core/CrossForgeException.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Core/SCrossForgeDevice.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Core/SCrossForgeDevice.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Core/SGPIO.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Core/SGPIO.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Core/SLogger.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Core/SLogger.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/Actors/IRenderableActor.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/Actors/IRenderableActor.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/Actors/RenderGroupUtility.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/Actors/RenderGroupUtility.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/Actors/ScreenQuad.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/Actors/ScreenQuad.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/Actors/StaticActor.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/Actors/StaticActor.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/Actors/VertexUtility.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/Actors/VertexUtility.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/GBuffer.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/GBuffer.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/GLBuffer.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/GLBuffer.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/GLTexture2D.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/GLTexture2D.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/GLVertexArray.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/GLVertexArray.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/GLWindow.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/GLWindow.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/GraphicsUtility.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/GraphicsUtility.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/Lights/DirectionalLight.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/Lights/DirectionalLight.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/Lights/ILight.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/Lights/ILight.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/Lights/PointLight.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/Lights/PointLight.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/Lights/SpotLight.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/Lights/SpotLight.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/RenderDevice.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/RenderDevice.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/RenderMaterial.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/RenderMaterial.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/STextureManager.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/STextureManager.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/SceneGraph/ISceneGraphNode.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/SceneGraph/ISceneGraphNode.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/SceneGraph/SGNGeometry.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/SceneGraph/SGNGeometry.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/SceneGraph/SGNTransformation.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/SceneGraph/SGNTransformation.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/SceneGraph/SceneGraph.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/SceneGraph/SceneGraph.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/Shader/GLShader.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/Shader/GLShader.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/Shader/SShaderManager.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/Shader/SShaderManager.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/Shader/ShaderCode.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/Shader/ShaderCode.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/UniformBufferObjects/UBOCameraData.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/UniformBufferObjects/UBOCameraData.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/UniformBufferObjects/UBOLightData.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/UniformBufferObjects/UBOLightData.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/UniformBufferObjects/UBOMaterialData.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/UniformBufferObjects/UBOMaterialData.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/UniformBufferObjects/UBOModelData.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/UniformBufferObjects/UBOModelData.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Graphics/VirtualCamera.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Graphics/VirtualCamera.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Input/Keyboard.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Input/Keyboard.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Input/Mouse.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Input/Mouse.cpp.o"
+  "/home/kurt/Documents/Code/C++/CrossForge/CForge/Input/SInputManager.cpp" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/CMakeFiles/crossforge.dir/CForge/Input/SInputManager.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "USE_SYSFS_GPIO"
+  "crossforge_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../thirdparty/glad/include"
+  "../thirdparty/eigen-3.4.0"
+  "/usr/include/opencv4"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/libcrossforge.so" "/home/kurt/Documents/Code/C++/CrossForge/cmake-build-debug/libcrossforge.so.0.1"
+  )
+
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
