1d7e5594cce2ab6dc3f4c0d1bada08cb0aee9e41
Tom Uhlmann <Tom.Uhlmann.87@GMail.com>
2021-11-30 11:38:16 +0100

    Update to Morph target animation controller

--- Prototypes/Actor/MorphTargetAnimationController.h
+++ Prototypes/Actor/MorphTargetAnimationController.h
@@ -19,20 +19,64 @@
 #define __CFORGE_MORPHTARGETANIMATIONCONTROLLER_H__
 
 #include "../../CForge/AssetIO/T3DMesh.hpp"
+#include "UBOMorphTargetData.h"
+
 
 
 namespace CForge {
-	class MorphTargetAnimationController {
+	class MorphTargetAnimationController: public CForgeObject {
 	public:
+		struct MorphTarget {
+			int32_t ID;
+			std::string Name;
+		};//MorphTarget
+
+		struct AnimationSequence {
+			int32_t ID;
+			std::string Name;
+			std::vector<int32_t> Targets; // sequence of targets
+			std::vector<Eigen::Vector3f> Parameters; // begin strength, end strength, duration in seconds
+
+			void clear(void) {
+				ID = -1;
+				Name = "";
+				Targets.clear();
+				Parameters.clear();
+			}
+		};//AnimationSequence
+
+		struct ActiveAnimation {
+			int32_t ID;
+			float Speed; ///< Progression speed
+			float t;	///< current stamp
+			int32_t SequenceID;
+			int32_t CurrentSquenceIndex;
+			uint64_t SequenceStartTimestamp;
+			bool Finished;
+		};
 
 		MorphTargetAnimationController(void);
 		~MorphTargetAnimationController(void);
 
 		void init(T3DMesh<float>* pMesh);
 		void clear(void);
+		void release(void);
+		void update(float FPSScale);
+
+		int32_t addAnimationSequence(AnimationSequence* pSequence);
+		ActiveAnimation* play(int32_t SequenceID, float Speed);
+
+		void apply(std::vector<ActiveAnimation*>* pAnims, UBOMorphTargetData* pUBO);
+		bool valid(ActiveAnimation* pAnim);
 
 	protected:
 
+		void progress(ActiveAnimation* pAnim, float Scale);
+
+		std::vector<MorphTarget*> m_MorphTargets;
+		std::vector<AnimationSequence*> m_AnimationSequences;
+
+		std::vector<ActiveAnimation*> m_ActiveAnimations;
 	};//MorphTargetAnimationController
 }//name space
 
