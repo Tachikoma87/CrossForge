8fbb43758b484f3d89602eba1c0ad7244deebd25
Skade-code <zocker1206@gmail.com>
2022-04-02 02:30:45 +0200

    added AABB rendering + query test

--- Prototypes/Actor/LODActor.cpp
+++ Prototypes/Actor/LODActor.cpp
@@ -4,6 +4,7 @@
 #include "../../CForge/Graphics/GraphicsUtility.h"
 #include "../../CForge/Core/SLogger.h"
 #include "../MeshDecimate.h"
+#include "../../CForge/Graphics/Shader/SShaderManager.h"
 
 namespace CForge {
 	LODActor::LODActor(void) : IRenderableActor("LODActor", ATYPE_STATIC) {
@@ -43,6 +44,7 @@ namespace CForge {
 			initiateBuffers(i);
 		}
 		
+		initAABB();
 	}//initialize
 
 	void LODActor::init(T3DMesh<float>* pMesh) {
@@ -184,9 +186,37 @@ namespace CForge {
 	
 	void LODActor::render(RenderDevice* pRDev) {
 		if (nullptr == pRDev) throw NullpointerExcept("pRDev");
+		
+		GLuint queryID;
+		glGenQueries(1, &queryID);
+		glBeginQuery(GL_SAMPLES_PASSED, queryID);
+		
+		glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
+		glDepthMask(GL_FALSE);
+		
+		//if (pRDev->activePass() == RenderDevice::RENDERPASS_LOD)
+		renderAABB(pRDev);
+		//return;
+		
+		glEndQuery(GL_SAMPLES_PASSED);
+		
+		// TODO abfrage im naechsten frame um wartezeit zu ueberbruecken
+		GLint queryState;
+		do {
+			glGetQueryObjectiv(queryID, GL_QUERY_RESULT_AVAILABLE, &queryState);
+		} while (!queryState);
+		
+		GLuint pixelCount;
+		glGetQueryObjectuiv(queryID, GL_QUERY_RESULT, &pixelCount);
+		glDeleteQueries(1, &queryID);
+		
+		std::cout << pixelCount << "\n";
+
+		glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+		glDepthMask(GL_TRUE);
 
 		m_VertexArray.bind();
-
+		
 		for (auto i : m_RenderGroupUtilities[m_LODLevel]->renderGroups()) {
 			if (i->pShader == nullptr) continue;
 
@@ -194,13 +224,128 @@ namespace CForge {
 				pRDev->activeShader(pRDev->shadowPassShader());
 			}
 			else {
+				// TODO Occlusion Culling
 				pRDev->activeShader(i->pShader);
 				pRDev->activeMaterial(&i->Material);
 			}
 			glDrawRangeElements(GL_TRIANGLES, 0, m_ElementBuffer.size() / sizeof(unsigned int), i->Range.y() - i->Range.x(), GL_UNSIGNED_INT, (const void*)(i->Range.x() * sizeof(unsigned int)));
 		}//for[all render groups]
-
+		
 	}//render
+	
+	void LODActor::initAABB() {
+		
+		// create Shader
+		const char* vertexShaderSource = "#version 330 core\n"
+			"layout(std140) uniform CameraData {\n"
+			"	mat4 ViewMatrix;\n"
+			"	mat4 ProjectionMatrix;\n"
+			"	vec4 Position;\n"
+			"} Camera;\n"
+			"layout(std140) uniform ModelData {\n"
+			"	mat4 ModelMatrix;\n"
+			"};\n"
+			"in vec3 VertPosition;\n"
+			"void main()\n"
+			"{\n"
+			"   gl_Position = Camera.ProjectionMatrix * Camera.ViewMatrix * ModelMatrix * vec4(VertPosition.x, VertPosition.y, VertPosition.z, 1.0);\n"
+			"}\0";
+		const char* fragmentShaderSource = "#version 330 core\n"
+			"out vec4 FragColor;\n"
+			"void main()\n"
+			"{\n"
+			"   FragColor = vec4(1.0);\n"
+			"}\n\0";
+
+		SShaderManager* shaderManager = SShaderManager::instance();
+		uint8_t ConfigOptions = 0;
+		ConfigOptions |= ShaderCode::CONF_VERTEXCOLORS;
+
+		ShaderCode* pVertC = shaderManager->createShaderCode(vertexShaderSource, "330", ConfigOptions, "highp", "highp");
+		ShaderCode* pFragC = shaderManager->createShaderCode(fragmentShaderSource, "330", ConfigOptions, "highp", "highp");
+		std::vector<ShaderCode*> VSSources;
+		std::vector<ShaderCode*> FSSources;
+		VSSources.push_back(pVertC);
+		FSSources.push_back(pFragC);
+		std::string buildShaderError;
+
+		m_AABBshader = shaderManager->buildShader(&VSSources, &FSSources, &buildShaderError);
+		//std::cout << buildShaderError << "\n";
+		shaderManager->release();
+		
+		// bind AABB to buffer
+		updateAABB();
+	}
+	
+	void LODActor::renderAABB(RenderDevice* pRDev) {
+		
+		pRDev->activeShader(m_AABBshader);
+		m_AABBvertArray.bind();
+		glDrawElements(GL_TRIANGLE_STRIP, 14, GL_UNSIGNED_INT, nullptr);
+		m_AABBvertArray.unbind();
+	}
+	
+	void LODActor::updateAABB() {
+		m_LODMeshes[0]->computeAxisAlignedBoundingBox(); // TODO unwanted side effect?
+		T3DMesh<float>::AABB aabb = m_LODMeshes[0]->aabb();
+		
+		//GLfloat vertices[] = { // Triangle Cube
+		//	aabb.Min.x(), aabb.Min.y(), aabb.Max.z(),
+		//	aabb.Min.x(), aabb.Max.y(), aabb.Max.z(),
+		//	aabb.Min.x(), aabb.Min.y(), aabb.Min.z(),
+		//	aabb.Min.x(), aabb.Max.y(), aabb.Min.z(),
+		//	aabb.Max.x(), aabb.Min.y(), aabb.Max.z(),
+		//	aabb.Max.x(), aabb.Max.y(), aabb.Max.z(),
+		//	aabb.Max.x(), aabb.Min.y(), aabb.Min.z(),
+		//	aabb.Max.x(), aabb.Max.y(), aabb.Min.z()
+		//};
+		//GLuint elements[] = {
+		//1, 2, 0,
+		//3, 6, 2,
+		//7, 4, 6,
+		//5, 0, 4,
+		//6, 0, 2,
+		//3, 5, 7,
+		//1, 3, 2,
+		//3, 7, 6,
+		//7, 5, 4,
+		//5, 1, 0,
+		//6, 4, 0,
+		//3, 1, 5
+		//};
+		
+		GLfloat vertices[] = { //Triangle Strip Cube
+			aabb.Min.x(), aabb.Min.y(), aabb.Min.z(),
+			aabb.Max.x(), aabb.Min.y(), aabb.Min.z(),
+			aabb.Min.x(), aabb.Max.y(), aabb.Min.z(),
+			aabb.Max.x(), aabb.Max.y(), aabb.Min.z(),
+			aabb.Min.x(), aabb.Min.y(), aabb.Max.z(),
+			aabb.Max.x(), aabb.Min.y(), aabb.Max.z(),
+			aabb.Max.x(), aabb.Max.y(), aabb.Max.z(),
+			aabb.Min.x(), aabb.Max.y(), aabb.Max.z()
+		};
+		GLuint elements[] = {
+			3,2,6,
+			7,4,2,
+			0,3,1,
+			6,5,4,
+			1,0
+		};
+
+		m_AABBvertArray.clear();
+		m_AABBvertArray.init();
+		m_AABBvertArray.bind();
+
+		m_AABBvertBuffer.init(GLBuffer::BTYPE_VERTEX, GLBuffer::BUSAGE_STATIC_DRAW, vertices, 24 * sizeof(GLfloat));
+		m_AABBindexBuffer.init(GLBuffer::BTYPE_INDEX, GLBuffer::BUSAGE_STATIC_DRAW, elements, 14 * sizeof(GLuint));
+		m_AABBvertBuffer.bind();
+		m_AABBindexBuffer.bind();
+
+		glEnableVertexAttribArray(GLShader::attribArrayIndex(GLShader::ATTRIB_POSITION));
+		glVertexAttribPointer(GLShader::attribArrayIndex(GLShader::ATTRIB_POSITION), 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+		m_AABBvertArray.unbind();
+	}
 	
 	bool LODActor::isTranslucent() {
 		return m_translucent;
