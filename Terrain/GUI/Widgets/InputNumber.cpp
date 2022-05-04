05e1e7b50de7266e6c23723dd8d3840fac01febb
Simon Kretzschmar <simon.kretzschmar@s2019.tu-chemnitz.de>
2022-05-04 16:01:23 +0200

    Implement a FormWidget with callback
    Implements a FormWidget that can group multiple inputs together and adds
    an "Apply" button that triggers the callback. For that to work, another
    Widget was added, the LabelWidget. It groups an input with its
    accompanying text label and provides a more abstract getValue function
    based on pointers instead of explicit values. Thus, the callback can use
    a single map for all its settings instead of handling each value type
    individually. The callback code only needs to cast the pointers back to
    their original/expected type.

--- Terrain/GUI/Widgets/InputNumber.cpp
+++ Terrain/GUI/Widgets/InputNumber.cpp
@@ -1,32 +1,5 @@
 #include "InputNumber.h"
 
-// TestWidget::TestWidget ( GUI* rootGUIObject, BaseWidget* parent ) : BaseWidget(rootGUIObject, parent)
-// {
-// //     m_background = new WidgetBackgroundColored();
-// //     BackgroundStyle b;
-// //     m_background->init(b, rootGUIObject->BackgroundColoredShader);
-//
-//     TextWidget* textTest = new TextWidget(rootGUIObject, this);
-//     textTest->setText(U"Zeile 1");
-//     m_children.push_back(textTest);
-//     textTest = new TextWidget(rootGUIObject, this);
-//     textTest->changePosition(0, 50);
-//     textTest->setText(U"und Zeile 2.");
-//     m_children.push_back(textTest);
-//
-//     //TODO: size reporting for text changes
-//     m_width = 200;
-//     m_height = 100;
-// }
-// void TestWidget::onClick ( CForge::Mouse* mouse )
-// {
-//     printf("reached here");
-//     changePosition(mouse->movement()[0],mouse->movement()[1]);
-//     for (auto x : m_children) {
-//         x->changePosition(mouse->movement()[0],mouse->movement()[1]);
-//     }
-// }
-
 InputNumberWidget_DecreaseButton::InputNumberWidget_DecreaseButton(GUI* rootGUIObject, InputNumberWidget* parent): TextWidget(rootGUIObject, parent)
 {
     //need to save the parent in an extra variable because we need access
@@ -101,43 +74,15 @@ void InputNumberWidget::changeValue(int value)
 }
 void InputNumberWidget::changePosition(float dx, float dy)
 {
+    m_x += dx;
+    m_y += dy;
     m_pDec->changePosition(dx, dy);
     m_pValue->changePosition(dx, dy);
     m_pInc->changePosition(dx, dy);
-}
-void InputNumberWidget::setPosition(float x, float y)
-{
-    float dx = x - m_x;
-    float dy = y - m_y;
-    m_x = x;
-    m_y = y;
-    changePosition(dx, dy);
 }
 void InputNumberWidget::draw(CForge::RenderDevice* renderDevice)
 {
     m_pDec->draw(renderDevice);
     m_pValue->draw(renderDevice);
     m_pInc->draw(renderDevice);
-}
-
-
-
-TestWidget::TestWidget ( GUI* rootGUIObject, BaseWidget* parent ) : BaseWidget(rootGUIObject, parent)
-{
-//     m_background = new WidgetBackgroundColored();
-//     BackgroundStyle b;
-//     m_background->init(b, rootGUIObject->BackgroundColoredShader);
-
-    InputNumberWidget* inputTest = new InputNumberWidget(rootGUIObject, this);
-    m_children.push_back(inputTest);
-
-    m_width = inputTest->getWidth();
-    m_height = inputTest->getHeight();
-}
-void TestWidget::onDrag ( mouseEventInfo mouse )
-{
-    setPosition(mouse.adjustedPosition[0], mouse.adjustedPosition[1]);
-    for (auto x : m_children) {
-        x->setPosition(mouse.adjustedPosition[0], mouse.adjustedPosition[1]);
-    }
 }
