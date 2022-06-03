306a96d894c1dc74b568c2d108884385b1de6038
Simon Kretzschmar <simon.kretzschmar@s2019.tu-chemnitz.de>
2022-06-03 16:16:27 +0200

    Add Border WidgetBackground
    Adds a variant of WidgetBackground that draws a simple border around its
    contents.

--- Terrain/GUI/GUIDefaults.h
+++ Terrain/GUI/GUIDefaults.h
@@ -4,12 +4,17 @@
 
 struct BackgroundStyle {
     float Alpha = 0.5f;
-    float Color[3] = {1.0f, 1.0f, 0.0f};
+    float Color[3] = {0.0f, 0.0f, 0.0f};
+};
+struct BorderStyle {
+    float LineWidth = 1.0f;
+    float Color[3] = {1.0f, 1.0f, 1.0f};
+    float Alpha = 1.0f;
 };
 
 struct FontStyle {
     std::string FileName = "Assets/DejaVuSans.ttf"; //Font file to load
-    int PixelSize = 40; //height of text in pixels
+    int PixelSize = 20; //height of text in pixels
     float FontColor[3] = {1.0f, 1.0f, 1.0f};
     float FontColorHighlight[3] = {1.0f, 1.0f, 0.5f};
 };
