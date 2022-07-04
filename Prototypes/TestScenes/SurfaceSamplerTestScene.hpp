637076cce215078395b608399319e029c5b123c4
Tom Uhlmann <Tom.Uhlmann.87@GMail.com>
2022-07-04 07:58:03 +0200

    Added Prototype of equidistant surface sampling

--- Prototypes/TestScenes/SurfaceSamplerTestScene.hpp
+++ Prototypes/TestScenes/SurfaceSamplerTestScene.hpp
@@ -1,8 +1,8 @@
 /*****************************************************************************\
 *                                                                           *
-* File(s): exampleSkeletalAnimation.hpp                                            *
+* File(s): SurfaceSamplerTestScene.hpp                                            *
 *                                                                           *
-* Content: Example scene that shows how to use skeletal animation.          *
+* Content:    *
 *                       *
 *                                                                           *
 *                                                                           *
@@ -15,39 +15,40 @@
 * supplied documentation.                                                   *
 *                                                                           *
 \****************************************************************************/
-#ifndef __CFORGE_EXAMPLESKELETALANIMATION_HPP__
-#define __CFORGE_EXAMPLESKELETALANIMATION_HPP__
+#ifndef __CFORGE_SURFACESAMPLERTESTSCENE_HPP__
+#define __CFORGE_SURFACESAMPLERTESTSCENE_HPP__
 
 
-#include "../CForge/AssetIO/SAssetIO.h"
-#include "../CForge/Graphics/Shader/SShaderManager.h"
-#include "../CForge/Graphics/STextureManager.h"
+#include "../../CForge/AssetIO/SAssetIO.h"
+#include "../../CForge/Graphics/Shader/SShaderManager.h"
+#include "../../CForge/Graphics/STextureManager.h"
 
-#include "../CForge/Graphics/GLWindow.h"
-#include "../CForge/Graphics/GraphicsUtility.h"
-#include "../CForge/Graphics/RenderDevice.h"
+#include "../../CForge/Graphics/GLWindow.h"
+#include "../../CForge/Graphics/GraphicsUtility.h"
+#include "../../CForge/Graphics/RenderDevice.h"
 
-#include "../CForge/Graphics/Lights/DirectionalLight.h"
-#include "../CForge/Graphics/Lights/PointLight.h"
+#include "../../CForge/Graphics/Lights/DirectionalLight.h"
+#include "../../CForge/Graphics/Lights/PointLight.h"
 
-#include "../CForge/Graphics/SceneGraph/SceneGraph.h"
-#include "../CForge/Graphics/SceneGraph/SGNGeometry.h"
-#include "../CForge/Graphics/SceneGraph/SGNTransformation.h"
+#include "../../CForge/Graphics/SceneGraph/SceneGraph.h"
+#include "../../CForge/Graphics/SceneGraph/SGNGeometry.h"
+#include "../../CForge/Graphics/SceneGraph/SGNTransformation.h"
 
-#include "../CForge/Graphics/Actors/StaticActor.h"
-#include "../CForge/Graphics/Actors/SkeletalActor.h"
+#include "../../CForge/Graphics/Actors/StaticActor.h"
 
-#include "SceneUtilities.hpp"
+#include "../../Examples/SceneUtilities.hpp"
+
+#include "../MeshProcessing/SurfaceSampler.h"
 
 using namespace Eigen;
 using namespace std;
 
 namespace CForge {
 
-	void exampleSkeletalAnimation(void) {
+	void surfaceSamplerTestScene(void) {
 		SShaderManager* pSMan = SShaderManager::instance();
 
-		std::string WindowTitle = "CForge - Skeletal Animation Example";
+		std::string WindowTitle = "CForge - Minimum Graphics Setup";
 		float FPS = 60.0f;
 
 		bool const LowRes = false;
@@ -83,27 +84,25 @@ namespace CForge {
 		LC.DirLightCount = 1;
 		LC.PointLightCount = 1;
 		LC.SpotLightCount = 0;
-		LC.PCFSize = 0;
-		LC.ShadowBias = 0.00001f;
+		LC.PCFSize = 1;
+		LC.ShadowBias = 0.0004f;
 		LC.ShadowMapCount = 1;
 		pSMan->configShader(LC);
 
 		// initialize camera
 		VirtualCamera Cam;
-		Cam.init(Vector3f(20.0f, 5.0f, 45.0f), Vector3f::UnitY());
-		Cam.lookAt(Vector3f(10.0f, 5.0f, 35.0f), Vector3f(0.0f, 4.0f, 25.0f), Vector3f::UnitY());
+		Cam.init(Vector3f(0.0f, 10.5f, 30.0f), Vector3f::UnitY());
 		Cam.projectionMatrix(WinWidth, WinHeight, GraphicsUtility::degToRad(45.0f), 0.1f, 1000.0f);
 
 		// initialize sun (key lights) and back ground light (fill light)
-		Vector3f SunPos = Vector3f(-25.0f, 50.0f, -20.0f);
-		Vector3f SunLookAt = Vector3f(0.0f, 0.0f, 30.0f);
-		Vector3f BGLightPos = Vector3f(0.0f, 5.0f, 60.0f);
+		Vector3f SunPos = Vector3f(-5.0f, 15.0f, 35.0f);
+		Vector3f BGLightPos = Vector3f(0.0f, 5.0f, -30.0f);
 		DirectionalLight Sun;
 		PointLight BGLight;
-		Sun.init(SunPos, (SunLookAt - SunPos).normalized(), Vector3f(1.0f, 1.0f, 1.0f), 5.0f);
+		Sun.init(SunPos, -SunPos.normalized(), Vector3f(1.0f, 1.0f, 1.0f), 5.0f);
 		// sun will cast shadows
-		Sun.initShadowCasting(1024, 1024, GraphicsUtility::orthographicProjection(20.0f, 20.0f, 0.1f, 1000.0f));
-		BGLight.init(BGLightPos, -BGLightPos.normalized(), Vector3f(1.0f, 1.0f, 1.0f), 5.5f, Vector3f(0.0f, 0.0f, 0.0f));
+		Sun.initShadowCasting(1024, 1024, GraphicsUtility::orthographicProjection(10.0f, 10.0f, 0.1f, 1000.0f));
+		BGLight.init(BGLightPos, -BGLightPos.normalized(), Vector3f(1.0f, 1.0f, 1.0f), 1.5f, Vector3f(0.0f, 0.0f, 0.0f));
 
 		// set camera and lights
 		RDev.activeCamera(&Cam);
@@ -111,25 +110,32 @@ namespace CForge {
 		RDev.addLight(&BGLight);
 
 		// load skydome and a textured cube
-		T3DMesh<float> M;	
+		T3DMesh<float> M;
 		StaticActor Skydome;
-		SkeletalActor Eric;
-		SkeletalAnimationController Controller;
+		StaticActor Surface;
+		SkeletalAnimationController MuscleManController;
+		StaticActor Sphere;
 
 		SAssetIO::load("Assets/ExampleScenes/SimpleSkydome.fbx", &M);
 		SceneUtilities::setMeshShader(&M, 0.8f, 0.04f);
 		M.computePerVertexNormals();
-		Skydome.init(&M);	
+		Skydome.init(&M);
 		M.clear();
-		
-		// initialize skeletal actor (Eric) and its animation controller
-		//SAssetIO::load("Assets/ExampleScenes/Eric_Anim.fbx", &M);
-		SAssetIO::load("Assets/tmp/MuscleManWalking.glb", &M);
-		SceneUtilities::setMeshShader(&M, 0.7f, 0.04f);
+
+		SAssetIO::load("Assets/tmp/Sphere.glb", &M);
+		SceneUtilities::setMeshShader(&M, 0.8f, 0.04f);
 		M.computePerVertexNormals();
-		Controller.init(&M);
-		Eric.init(&M, &Controller);
+		for (uint32_t i = 0; i < M.materialCount(); ++i) M.getMaterial(i)->Color = Vector4f(0.0f, 0.0f, 1.0f, 1.0f); // red spheres
+		Sphere.init(&M);
 		M.clear();
+
+		SAssetIO::load("Assets/tmp/MuscleManPosed.obj", &M);
+		SceneUtilities::setMeshShader(&M, 0.1f, 0.04f);
+		M.computePerVertexNormals();
+		Surface.init(&M);
+
+		// don't clear M, we need the data later
+		//M.clear();
 
 		// build scene graph
 		SceneGraph SG;
@@ -137,46 +143,69 @@ namespace CForge {
 		RootSGN.init(nullptr);
 		SG.init(&RootSGN);
 
+
 		// add skydome
 		SGNGeometry SkydomeSGN;
 		SkydomeSGN.init(&RootSGN, &Skydome);
 		SkydomeSGN.scale(Vector3f(5.0f, 5.0f, 5.0f));
 
-		// add skeletal actor to scene graph (Eric)
-		SGNGeometry EricSGN;
-		SGNTransformation EricTransformSGN;
-		EricTransformSGN.init(&RootSGN, Vector3f(0.0f, 0.0f, 0.0f));
-		EricSGN.init(&EricTransformSGN, &Eric);
-		EricSGN.scale(Vector3f(0.05f, 0.05f, 0.05f));
+
+		// add muscle man
+		SGNGeometry MuscleManSGN;
+		SGNTransformation MuscleManTransformSGN;
+		MuscleManTransformSGN.init(&RootSGN, Vector3f(0.0f, 10.0f, 0.0f));
+		MuscleManTransformSGN.scale(Vector3f(1.0f/10.0f, 1.0f/10.0f, 1.0f/10.0f));
+		MuscleManSGN.init(&MuscleManTransformSGN, &Surface);
+
+
+		// rotate about the y-axis at 45 degree every second
+		Quaternionf R = Quaternionf::Identity();
+		R = AngleAxisf(GraphicsUtility::degToRad(45.0f / 60.0f), Vector3f::UnitY());
+		
+		MuscleManTransformSGN.rotationDelta(R);
+
+		// prepare node to add markers
+		SGNTransformation MarkerGroupSGN;
+		MarkerGroupSGN.init(&MuscleManTransformSGN); // markers will always belong to this object for the purpose of this demo
 
 		// stuff for performance monitoring
 		uint64_t LastFPSPrint = CoreUtility::timestamp();
 		int32_t FPSCount = 0;
 
-		// check wheter a GL error occurred
 		std::string GLError = "";
 		GraphicsUtility::checkGLError(&GLError);
 		if (!GLError.empty()) printf("GLError occurred: %s\n", GLError.c_str());
 
-		// start main loop
+
+		// setup for the surface sampler
+		SurfaceSampler SSampler;
+		// we use MuscleMan data already stored in M
+		SSampler.init(&M);
+
+
 		while (!RenderWin.shutdown()) {
 			RenderWin.update();
-			SG.update(60.0f/FPS);
+			SG.update(60.0f / FPS);
 
-			// this will progress all active skeletal animations for this controller
-			Controller.update(FPS / 60.0f);
+
+			if (RenderWin.keyboard()->keyPressed(Keyboard::KEY_0, true)) {
+				float MaxDist = 0.5f;
+				std::vector<int32_t> Samples;
+				SSampler.sampleEquidistant(MaxDist, 50, &Samples);
+
+				// add marker at position of sample point
+				for (auto i : Samples) {
+					SGNTransformation* pMarkerSGN = new SGNTransformation();
+					SGNTransformation* pMarkerSGN2 = new SGNTransformation();
+					SGNGeometry* pMarkerGeomSGN = new SGNGeometry();
+
+					pMarkerSGN->init(&MarkerGroupSGN, M.vertex(i).cwiseProduct(MuscleManTransformSGN.scale()));
+					pMarkerSGN2->init(pMarkerSGN, Vector3f::Zero(), MuscleManTransformSGN.rotation(), MuscleManTransformSGN.scale(), Vector3f::Zero(), MuscleManTransformSGN.rotationDelta());
+					pMarkerGeomSGN->init(pMarkerSGN2, &Sphere, Vector3f::Zero(), Quaternionf::Identity(), Vector3f(5.0f, 5.0f, 5.0f));		
+				}
+			}
 
 			SceneUtilities::defaultCameraUpdate(&Cam, RenderWin.keyboard(), RenderWin.mouse());
-
-			// if user hits key 1, animation will be played
-			// if user also presses shift, animation speed is doubled
-			float AnimationSpeed = 1.0f;
-			if (RenderWin.keyboard()->keyPressed(Keyboard::KEY_LEFT_SHIFT)) AnimationSpeed *= 2.0f;
-			if (RenderWin.keyboard()->keyPressed(Keyboard::KEY_LEFT_CONTROL)) AnimationSpeed *= 0.25f;
-			if (RenderWin.keyboard()->keyPressed(Keyboard::KEY_1, true)) {
-				SkeletalAnimationController::Animation *pAnim = Controller.createAnimation(0, AnimationSpeed, 0.0f);
-				Eric.activeAnimation(pAnim);
-			}
 
 			RDev.activePass(RenderDevice::RENDERPASS_SHADOW, &Sun);
 			SG.render(&RDev);
