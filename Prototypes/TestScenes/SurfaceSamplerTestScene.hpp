7816a0f6c5d9446bb2c0893d6ad5b893c10d041e
Tom Uhlmann <Tom.Uhlmann.87@GMail.com>
2022-07-23 23:39:46 +0200

    Refactored all prototype scenes to new class structure

--- Prototypes/TestScenes/SurfaceSamplerTestScene.hpp
+++ Prototypes/TestScenes/SurfaceSamplerTestScene.hpp
@@ -18,26 +18,7 @@
 #ifndef __CFORGE_SURFACESAMPLERTESTSCENE_HPP__
 #define __CFORGE_SURFACESAMPLERTESTSCENE_HPP__
 
-
-#include "../../CForge/AssetIO/SAssetIO.h"
-#include "../../CForge/Graphics/Shader/SShaderManager.h"
-#include "../../CForge/Graphics/STextureManager.h"
-
-#include "../../CForge/Graphics/GLWindow.h"
-#include "../../CForge/Graphics/GraphicsUtility.h"
-#include "../../CForge/Graphics/RenderDevice.h"
-
-#include "../../CForge/Graphics/Lights/DirectionalLight.h"
-#include "../../CForge/Graphics/Lights/PointLight.h"
-
-#include "../../CForge/Graphics/SceneGraph/SceneGraph.h"
-#include "../../CForge/Graphics/SceneGraph/SGNGeometry.h"
-#include "../../CForge/Graphics/SceneGraph/SGNTransformation.h"
-
-#include "../../CForge/Graphics/Actors/StaticActor.h"
-
-#include "../../Examples/SceneUtilities.hpp"
-
+#include "../../Examples/exampleSceneBase.hpp"
 #include "../MeshProcessing/SurfaceSampler.h"
 
 using namespace Eigen;
@@ -45,195 +26,157 @@ using namespace std;
 
 namespace CForge {
 
-	void surfaceSamplerTestScene(void) {
-		SShaderManager* pSMan = SShaderManager::instance();
+	class SurfaceSamplerTestScene : public ExampleSceneBase {
+	public:
+		SurfaceSamplerTestScene(void) {
+			m_WindowTitle = "CForge Prototype - Surface Sampler";
+			m_SampleCount = 10;
+		}//Constructor
 
-		std::string WindowTitle = "CForge - Minimum Graphics Setup";
-		float FPS = 60.0f;
+		~SurfaceSamplerTestScene(void) {
+			clear();
+		}//Destructor
 
-		bool const LowRes = false;
+		void init(void) {
+			initWindowAndRenderDevice();
+			initCameraAndLights();
 
-		uint32_t WinWidth = 1280;
-		uint32_t WinHeight = 720;
+			T3DMesh<float> M;
+			
 
-		if (LowRes) {
-			WinWidth = 720;
-			WinHeight = 576;
-		}
+			SAssetIO::load("Assets/ExampleScenes/SimpleSkydome.fbx", &M);
+			setMeshShader(&M, 0.8f, 0.04f);
+			M.computePerVertexNormals();
+			m_Skydome.init(&M);
+			M.clear();
 
-		// create an OpenGL capable windows
-		GLWindow RenderWin;
-		RenderWin.init(Vector2i(100, 100), Vector2i(WinWidth, WinHeight), WindowTitle);
+			SAssetIO::load("Assets/tmp/Sphere.glb", &M);
+			setMeshShader(&M, 0.8f, 0.04f);
+			M.computePerVertexNormals();
+			for (uint32_t i = 0; i < M.materialCount(); ++i) M.getMaterial(i)->Color = Vector4f(0.0f, 0.0f, 1.0f, 1.0f); // red spheres
+			m_Sphere.init(&M);
+			M.clear();
 
-		// configure and initialize rendering pipeline
-		RenderDevice RDev;
-		RenderDevice::RenderDeviceConfig Config;
-		Config.DirectionalLightsCount = 1;
-		Config.PointLightsCount = 1;
-		Config.SpotLightsCount = 0;
-		Config.ExecuteLightingPass = true;
-		Config.GBufferHeight = WinHeight;
-		Config.GBufferWidth = WinWidth;
-		Config.pAttachedWindow = &RenderWin;
-		Config.PhysicallyBasedShading = true;
-		Config.UseGBuffer = true;
-		RDev.init(&Config);
-
-		// configure and initialize shader configuration device
-		ShaderCode::LightConfig LC;
-		LC.DirLightCount = 1;
-		LC.PointLightCount = 1;
-		LC.SpotLightCount = 0;
-		LC.PCFSize = 1;
-		LC.ShadowBias = 0.0004f;
-		LC.ShadowMapCount = 1;
-		pSMan->configShader(LC);
-
-		// initialize camera
-		VirtualCamera Cam;
-		Cam.init(Vector3f(0.0f, 10.5f, 30.0f), Vector3f::UnitY());
-		Cam.projectionMatrix(WinWidth, WinHeight, GraphicsUtility::degToRad(45.0f), 0.1f, 1000.0f);
-
-		// initialize sun (key lights) and back ground light (fill light)
-		Vector3f SunPos = Vector3f(-5.0f, 15.0f, 35.0f);
-		Vector3f BGLightPos = Vector3f(0.0f, 5.0f, -30.0f);
-		DirectionalLight Sun;
-		PointLight BGLight;
-		Sun.init(SunPos, -SunPos.normalized(), Vector3f(1.0f, 1.0f, 1.0f), 5.0f);
-		// sun will cast shadows
-		Sun.initShadowCasting(1024, 1024, GraphicsUtility::orthographicProjection(10.0f, 10.0f, 0.1f, 1000.0f));
-		BGLight.init(BGLightPos, -BGLightPos.normalized(), Vector3f(1.0f, 1.0f, 1.0f), 1.5f, Vector3f(0.0f, 0.0f, 0.0f));
-
-		// set camera and lights
-		RDev.activeCamera(&Cam);
-		RDev.addLight(&Sun);
-		RDev.addLight(&BGLight);
-
-		// load skydome and a textured cube
-		T3DMesh<float> M;
-		StaticActor Skydome;
-		StaticActor Surface;
-		SkeletalAnimationController MuscleManController;
-		StaticActor Sphere;
-
-		SAssetIO::load("Assets/ExampleScenes/SimpleSkydome.fbx", &M);
-		SceneUtilities::setMeshShader(&M, 0.8f, 0.04f);
-		M.computePerVertexNormals();
-		Skydome.init(&M);
-		M.clear();
-
-		SAssetIO::load("Assets/tmp/Sphere.glb", &M);
-		SceneUtilities::setMeshShader(&M, 0.8f, 0.04f);
-		M.computePerVertexNormals();
-		for (uint32_t i = 0; i < M.materialCount(); ++i) M.getMaterial(i)->Color = Vector4f(0.0f, 0.0f, 1.0f, 1.0f); // red spheres
-		Sphere.init(&M);
-		M.clear();
-
-		SAssetIO::load("Assets/tmp/MuscleManPosed.obj", &M);
-		SceneUtilities::setMeshShader(&M, 0.1f, 0.04f);
-		M.computePerVertexNormals();
-		Surface.init(&M);
-
-		// don't clear M, we need the data later
-		//M.clear();
-
-		// build scene graph
-		SceneGraph SG;
-		SGNTransformation RootSGN;
-		RootSGN.init(nullptr);
-		SG.init(&RootSGN);
+			SAssetIO::load("Assets/tmp/MuscleManPosed.obj", &m_ModelData);
+			setMeshShader(&m_ModelData, 0.1f, 0.04f);
+			m_ModelData.computePerVertexNormals();
+			m_Surface.init(&m_ModelData);
 
 
-		// add skydome
-		SGNGeometry SkydomeSGN;
-		SkydomeSGN.init(&RootSGN, &Skydome);
-		SkydomeSGN.scale(Vector3f(5.0f, 5.0f, 5.0f));
+			// build scene graph	
+			m_RootSGN.init(nullptr);
+			m_SG.init(&m_RootSGN);
+
+			// add skydome
+			m_SkydomeSGN.init(&m_RootSGN, &m_Skydome);
+			m_SkydomeSGN.scale(Vector3f(5.0f, 5.0f, 5.0f));
+
+			// add muscle man		
+			m_MuscleManTransformSGN.init(&m_RootSGN, Vector3f(0.0f, 3.0f, 0.0f));
+			m_MuscleManTransformSGN.scale(Vector3f(1.0f / 40.0f, 1.0f / 40.0f, 1.0f / 40.0f));
+			m_MuscleManSGN.init(&m_MuscleManTransformSGN, &m_Surface);
+
+			// rotate about the y-axis at 45 degree every second
+			Quaternionf R = Quaternionf::Identity();
+			R = AngleAxisf(GraphicsUtility::degToRad(45.0f / 60.0f), Vector3f::UnitY());
+
+			m_MuscleManTransformSGN.rotationDelta(R);
+
+			// prepare node to add markers		
+			m_MarkerGroupSGN.init(&m_MuscleManTransformSGN); // markers will always belong to this object for the purpose of this demo
+
+			// stuff for performance monitoring
+			uint64_t LastFPSPrint = CoreUtility::timestamp();
+			int32_t FPSCount = 0;
+
+			std::string GLError = "";
+			GraphicsUtility::checkGLError(&GLError);
+			if (!GLError.empty()) printf("GLError occurred: %s\n", GLError.c_str());
+
+			// setup for the surface sampler
+			
+			// we use MuscleMan data already stored in M
+			m_SSampler.init(&m_ModelData);
+
+		}//initialize
+
+		void clear(void) {
+			ExampleSceneBase::clear();
+		}//clear
+
+		void run(void) {
+
+			m_FPS = 60.0f;
+			m_FPSCount = 0;
+			m_LastFPSPrint = CoreUtility::timestamp();
+
+			while (!m_RenderWin.shutdown()) {
+				m_RenderWin.update();
+				m_SG.update(60.0f / m_FPS);
 
 
-		// add muscle man
-		SGNGeometry MuscleManSGN;
-		SGNTransformation MuscleManTransformSGN;
-		MuscleManTransformSGN.init(&RootSGN, Vector3f(0.0f, 10.0f, 0.0f));
-		MuscleManTransformSGN.scale(Vector3f(1.0f/10.0f, 1.0f/10.0f, 1.0f/10.0f));
-		MuscleManSGN.init(&MuscleManTransformSGN, &Surface);
+				if (m_RenderWin.keyboard()->keyPressed(Keyboard::KEY_0, true)) {
+					float MaxDist = 0.5f;
+					std::vector<int32_t> Samples;
+					m_SSampler.sampleEquidistant(MaxDist, m_SampleCount, &Samples);
 
+					// add marker at position of sample point
+					for (auto i : Samples) {
+						SGNTransformation* pMarkerSGN = new SGNTransformation();
+						SGNTransformation* pMarkerSGN2 = new SGNTransformation();
+						SGNGeometry* pMarkerGeomSGN = new SGNGeometry();
 
-		// rotate about the y-axis at 45 degree every second
-		Quaternionf R = Quaternionf::Identity();
-		R = AngleAxisf(GraphicsUtility::degToRad(45.0f / 60.0f), Vector3f::UnitY());
-		
-		MuscleManTransformSGN.rotationDelta(R);
-
-		// prepare node to add markers
-		SGNTransformation MarkerGroupSGN;
-		MarkerGroupSGN.init(&MuscleManTransformSGN); // markers will always belong to this object for the purpose of this demo
-
-		// stuff for performance monitoring
-		uint64_t LastFPSPrint = CoreUtility::timestamp();
-		int32_t FPSCount = 0;
-
-		std::string GLError = "";
-		GraphicsUtility::checkGLError(&GLError);
-		if (!GLError.empty()) printf("GLError occurred: %s\n", GLError.c_str());
-
-
-		// setup for the surface sampler
-		SurfaceSampler SSampler;
-		// we use MuscleMan data already stored in M
-		SSampler.init(&M);
-
-
-		while (!RenderWin.shutdown()) {
-			RenderWin.update();
-			SG.update(60.0f / FPS);
-
-
-			if (RenderWin.keyboard()->keyPressed(Keyboard::KEY_0, true)) {
-				float MaxDist = 0.5f;
-				std::vector<int32_t> Samples;
-				SSampler.sampleEquidistant(MaxDist, 50, &Samples);
-
-				// add marker at position of sample point
-				for (auto i : Samples) {
-					SGNTransformation* pMarkerSGN = new SGNTransformation();
-					SGNTransformation* pMarkerSGN2 = new SGNTransformation();
-					SGNGeometry* pMarkerGeomSGN = new SGNGeometry();
-
-					pMarkerSGN->init(&MarkerGroupSGN, M.vertex(i).cwiseProduct(MuscleManTransformSGN.scale()));
-					pMarkerSGN2->init(pMarkerSGN, Vector3f::Zero(), MuscleManTransformSGN.rotation(), MuscleManTransformSGN.scale(), Vector3f::Zero(), MuscleManTransformSGN.rotationDelta());
-					pMarkerGeomSGN->init(pMarkerSGN2, &Sphere, Vector3f::Zero(), Quaternionf::Identity(), Vector3f(5.0f, 5.0f, 5.0f));		
+						pMarkerSGN->init(&m_MarkerGroupSGN, m_ModelData.vertex(i).cwiseProduct(m_MuscleManTransformSGN.scale()));
+						pMarkerSGN2->init(pMarkerSGN, Vector3f::Zero(), m_MuscleManTransformSGN.rotation(), m_MuscleManTransformSGN.scale(), Vector3f::Zero(), m_MuscleManTransformSGN.rotationDelta());
+						pMarkerGeomSGN->init(pMarkerSGN2, &m_Sphere, Vector3f::Zero(), Quaternionf::Identity(), Vector3f(25.0f, 25.0f, 25.0f));
+					}
 				}
-			}
 
-			SceneUtilities::defaultCameraUpdate(&Cam, RenderWin.keyboard(), RenderWin.mouse());
+				defaultCameraUpdate(&m_Cam, m_RenderWin.keyboard(), m_RenderWin.mouse());
 
-			RDev.activePass(RenderDevice::RENDERPASS_SHADOW, &Sun);
-			SG.render(&RDev);
+				m_RenderDev.activePass(RenderDevice::RENDERPASS_SHADOW, &m_Sun);
+				m_SG.render(&m_RenderDev);
 
-			RDev.activePass(RenderDevice::RENDERPASS_GEOMETRY);
-			SG.render(&RDev);
+				m_RenderDev.activePass(RenderDevice::RENDERPASS_GEOMETRY);
+				m_SG.render(&m_RenderDev);
 
-			RDev.activePass(RenderDevice::RENDERPASS_LIGHTING);
+				m_RenderDev.activePass(RenderDevice::RENDERPASS_LIGHTING);
 
-			RenderWin.swapBuffers();
+				m_RenderWin.swapBuffers();
 
-			FPSCount++;
-			if (CoreUtility::timestamp() - LastFPSPrint > 1000U) {
-				char Buf[64];
-				sprintf(Buf, "FPS: %d\n", FPSCount);
-				FPS = float(FPSCount);
-				FPSCount = 0;
-				LastFPSPrint = CoreUtility::timestamp();
+				updateFPS();
+				defaultKeyboardUpdate(m_RenderWin.keyboard());
+			}//while[main loop]
+		}//run
 
-				RenderWin.title(WindowTitle + "[" + std::string(Buf) + "]");
-			}
+	protected:
+		StaticActor m_Skydome;
+		StaticActor m_Surface;
+		SkeletalAnimationController m_MuscleManController;
+		StaticActor m_Sphere;
 
-			if (RenderWin.keyboard()->keyPressed(Keyboard::KEY_ESCAPE)) {
-				RenderWin.closeWindow();
-			}
-		}//while[main loop]
+		SurfaceSampler m_SSampler;
 
-		pSMan->release();
+		SGNTransformation m_RootSGN;
+
+		SGNGeometry m_SkydomeSGN;
+		SGNGeometry m_MuscleManSGN;
+		SGNTransformation m_MuscleManTransformSGN;
+
+		SGNTransformation m_MarkerGroupSGN;
+
+		T3DMesh<float> m_ModelData;
+
+		uint32_t m_SampleCount;
+	};//SurfaceSamplerTestScene
+
+
+	void surfaceSamplerTestScene(void) {
+
+		SurfaceSamplerTestScene Scene;
+		Scene.init();
+		Scene.run();
+		Scene.clear();
 
 	}//exampleMinimumGraphicsSetup
 
