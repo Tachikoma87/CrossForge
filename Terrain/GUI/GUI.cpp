70d6605eec4e9273c27bf70cd2e380364132f1f2
Simon Kretzschmar <simon.kretzschmar@s2019.tu-chemnitz.de>
2022-08-05 11:01:37 +0200

    Apply stashed changes

--- Terrain/GUI/GUI.cpp
+++ Terrain/GUI/GUI.cpp
@@ -169,6 +169,28 @@ TextWidget * GUI::createPlainText()
     submitTopLevelWidget(text);
     return text;
 }
+TextWidget * GUI::createTextWindow(std::u32string title)
+{
+    WindowWidget* window = new WindowWidget(title, this, nullptr);
+    TextWidget* text = new TextWidget(this, nullptr);
+    window->setContentWidget(text);
+    submitTopLevelWidget(window);
+    //try to not have them overlap by default
+    float x = 0;
+    float y = 0;
+    float h = 0;
+    for (int i = 0; i < m_TopLevelWidgets.size() - 1; i++) {
+        x += m_TopLevelWidgets[i]->pWidget->getWidth();
+        h = std::max(h, m_TopLevelWidgets[i]->pWidget->getWidth());
+        if (x > m_pWin->width()) {
+            x = 0;
+            y += h;
+            h = 0;
+        };
+    }
+    window->setPosition(x, y);
+    return text;
+}
 
 void GUI::submitTopLevelWidget(BaseWidget* widget)
 {
