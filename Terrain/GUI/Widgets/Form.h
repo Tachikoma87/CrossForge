9a2085af5f87b7535ddba926e4e344445ecb4933
Tom Uhlmann <Tom.Uhlmann.87@GMail.com>
2022-09-30 23:17:58 +0200

    Namespace, Filetag, Headerguards for GUI files

--- Terrain/GUI/Widgets/Form.h
+++ Terrain/GUI/Widgets/Form.h
@@ -1,54 +1,75 @@
-#pragma once
+/*****************************************************************************\
+*                                                                           *
+* File(s): Form.h and Form.cpp                                      *
+*                                                                           *
+* Content:    *
+*          .                                         *
+*                                                                           *
+*                                                                           *
+* Author(s): Simon Kretzschmar, Tom Uhlmann                                                    *
+*                                                                           *
+*                                                                           *
+* The file(s) mentioned above are provided as is under the terms of the     *
+* FreeBSD License without any warranty or guaranty to work properly.        *
+* For additional license, copyright and contact/support issues see the      *
+* supplied documentation.                                                   *
+*                                                                           *
+\****************************************************************************/
+#ifndef __CFORGE_FORM_H__
+#define __CFORGE_FORM_H__
 
 #include "../Widget.h"
 #include "../GUI.h"
-#include "../Callback.h"
 
 #include "Label.h"
 #include <CForge/Core/ITCaller.hpp>
 
-class FormWidget;
+namespace CForge {
 
-class FormWidget_ApplyButton : public TextWidget {
-public:
-    FormWidget_ApplyButton(GUI* rootGUIObject, FormWidget* parent);
-    void onClick(mouseEventInfo) override;
-private:
-    FormWidget* m_parentForm;
-};
+    class FormWidget;
 
-class FormWidget : public BaseWidget, public CForge::ITCaller<GUICallbackObject> {
-public:
-    FormWidget(int FormID, GUI* rootGUIObject, BaseWidget* parent, std::u32string applyName = U"Apply");
-    ~FormWidget();
+    class FormWidget_ApplyButton : public TextWidget {
+    public:
+        FormWidget_ApplyButton(GUI* rootGUIObject, FormWidget* parent);
+        void onClick(mouseEventInfo) override;
+    private:
+        FormWidget* m_parentForm;
+    };
 
-    void addOption(int OptionID, GUIInputType type, std::u32string name);
-//     template <typename T>
-//     auto addOption(int OptionID, GUIInputType type, std::u32string name);
-    void setLimit(int OptionID, int higher);
-    void setLimit(int OptionID, int lower, int higher);
-    void setLimit(int OptionID, float higher);
-    void setLimit(int OptionID, float lower, float higher);
-    void setDefault(int OptionID, int value);
-    void setDefault(int OptionID, float value);
-    void setDefault(int OptionID, bool value);
-    void setDefault(int OptionID, std::u32string value);
-    void setStepSize(int OptionID, float stepSize);
-    void setDropDownOptions(int OptionID, std::map<int, std::u32string> optionMap);
-    void sendCallback();
+    class FormWidget : public BaseWidget, public CForge::ITCaller<GUICallbackObject> {
+    public:
+        FormWidget(int FormID, GUI* rootGUIObject, BaseWidget* parent, std::u32string applyName = U"Apply");
+        ~FormWidget();
 
-    void childValueChanged(BaseWidget* child) override;
+        void addOption(int OptionID, GUIInputType type, std::u32string name);
+        //     template <typename T>
+        //     auto addOption(int OptionID, GUIInputType type, std::u32string name);
+        void setLimit(int OptionID, int higher);
+        void setLimit(int OptionID, int lower, int higher);
+        void setLimit(int OptionID, float higher);
+        void setLimit(int OptionID, float lower, float higher);
+        void setDefault(int OptionID, int value);
+        void setDefault(int OptionID, float value);
+        void setDefault(int OptionID, bool value);
+        void setDefault(int OptionID, std::u32string value);
+        void setStepSize(int OptionID, float stepSize);
+        void setDropDownOptions(int OptionID, std::map<int, std::u32string> optionMap);
+        void sendCallback();
 
-    void changePosition(float dx, float dy) override;
-    void updateLayout() override;
-    void draw(CForge::RenderDevice* renderDevice) override;
+        void childValueChanged(BaseWidget* child) override;
 
-    //for testing
-    void onDrag(mouseEventInfo mouse) override;
-private:
-    int m_FormID;
-    std::unordered_map<int, LabelWidget*> m_labels;
-    FormWidget_ApplyButton* m_ApplyButton;
-    bool dontSendCallback = false;      //used for immediate callback mode to prevent calling it unnecessarily often
-};
+        void changePosition(float dx, float dy) override;
+        void updateLayout() override;
+        void draw(CForge::RenderDevice* renderDevice) override;
 
+        //for testing
+        void onDrag(mouseEventInfo mouse) override;
+    private:
+        int m_FormID;
+        std::unordered_map<int, LabelWidget*> m_labels;
+        FormWidget_ApplyButton* m_ApplyButton;
+        bool dontSendCallback = false;      //used for immediate callback mode to prevent calling it unnecessarily often
+    };
+
+}//name space
+#endif
--- Terrain/GUI/Widgets/Label.h
+++ Terrain/GUI/Widgets/Label.h
@@ -1,8 +1,25 @@
-#pragma once
+/*****************************************************************************\
+*                                                                           *
+* File(s): Label.h and Label.cpp                                      *
+*                                                                           *
+* Content:    *
+*          .                                         *
+*                                                                           *
+*                                                                           *
+* Author(s): Simon Kretzschmar, Tom Uhlmann                                                    *
+*                                                                           *
+*                                                                           *
+* The file(s) mentioned above are provided as is under the terms of the     *
+* FreeBSD License without any warranty or guaranty to work properly.        *
+* For additional license, copyright and contact/support issues see the      *
+* supplied documentation.                                                   *
+*                                                                           *
+\****************************************************************************/
+#ifndef __CFORGE_LABEL_H__
+#define __CFORGE_LABEL_H__
 
 #include "../Widget.h"
 #include "../GUI.h"
-#include "../Callback.h"
 
 #include "InputNumber.h"
 #include "InputCheckbox.h"
@@ -10,35 +27,40 @@
 #include "InputSlider.h"
 #include "InputDropDown.h"
 
-class LabelWidget : public BaseWidget {
-public:
-    LabelWidget(GUIInputType type, std::u32string labelText, GUI* rootGUIObject, BaseWidget* parent);
-    ~LabelWidget();
+namespace CForge {
 
-    GUICallbackDatum getValue();
-    void setLimit(int lower, int higher);
-    void setLimit(float lower, float higher);
-    void setDefault(int value);
-    void setDefault(float value);
-    void setDefault(bool value);
-    void setDefault(std::u32string value);
-    void setStepSize(float stepSize);
-    void setOptions(std::map<int, std::u32string> optionMap);
+    class LabelWidget : public BaseWidget {
+    public:
+        LabelWidget(GUIInputType type, std::u32string labelText, GUI* rootGUIObject, BaseWidget* parent);
+        ~LabelWidget();
 
-//     template <typename T> auto getInputWidget();
+        GUICallbackDatum getValue();
+        void setLimit(int lower, int higher);
+        void setLimit(float lower, float higher);
+        void setDefault(int value);
+        void setDefault(float value);
+        void setDefault(bool value);
+        void setDefault(std::u32string value);
+        void setStepSize(float stepSize);
+        void setOptions(std::map<int, std::u32string> optionMap);
 
-    float getJustification();
-    void setJustification(float j);
+        //     template <typename T> auto getInputWidget();
 
-    void childValueChanged(BaseWidget* child) override;
+        float getJustification();
+        void setJustification(float j);
 
-    void changePosition(float dx, float dy) override;
-    void updateLayout() override;
-    void draw(CForge::RenderDevice* renderDevice) override;
-private:
-    GUIInputType m_type;
-    void * m_pValue;
-    TextWidget* m_pLabelText;
-    BaseWidget* m_pInput;
-    float m_justification;      //offset of the input element
-};
+        void childValueChanged(BaseWidget* child) override;
+
+        void changePosition(float dx, float dy) override;
+        void updateLayout() override;
+        void draw(CForge::RenderDevice* renderDevice) override;
+    private:
+        GUIInputType m_type;
+        void* m_pValue;
+        TextWidget* m_pLabelText;
+        BaseWidget* m_pInput;
+        float m_justification;      //offset of the input element
+    };
+
+}//name space
+#endif
