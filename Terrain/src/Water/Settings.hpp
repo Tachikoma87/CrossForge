81aeb05b63a175221f5d476e703aefe529d8ec68
Richard Wolf <cavespidermensch@gmail.com>
2022-12-04 09:38:11 +0100

    - quality option now selectable with "Assets/qualitySelection.txt" - adjusted compute shader code to run on older systems

--- Terrain/src/Water/Settings.hpp
+++ Terrain/src/Water/Settings.hpp
@@ -9,32 +9,40 @@ enum presetOption {
 	CUSTOM
 };
 
+presetOption loadSettings() {
+	std::ifstream infile("Assets/qualitySelection.txt");
+	std::string userSetting;
+	std::getline(infile, userSetting);
+	infile.close();
+	std::cout << userSetting << "\n\n";
+
+	if (userSetting == "min") return MIN;
+	if (userSetting == "low") return LOW;
+	if (userSetting == "med") return MED;
+	if (userSetting == "hig") return HIG;
+	if (userSetting == "max") return MAX;
+	if (userSetting == "custom") return CUSTOM;
+	std::cout << "False quality Option!";
+	return MIN;
+}
+
 // select graphics setting preset or custom settings
-presetOption selectedPreset = LOW;
+presetOption selectedPreset = loadSettings();
 
 // adjust values for custom settings here:
 struct settings {
-	int windowWidth = 1920;// 5120 / 4;		// window width
-	int windowHeight = 1080;// 1440 / 2;	// window height
-	bool ssr = true;				// screen space reflection
+	int windowWidth = 1280;// 5120 / 4;		// window width
+	int windowHeight = 720;// 1440 / 2;	// window height
+	bool ssr = false;				// screen space reflection
 	float ssrResolution = 0.75;		// has to be bigger than 0 and the lower the value the cheaper the SSR is.
-	bool blur = true;				// gaussian blur on water reflections
+	bool blur = false;				// gaussian blur on water reflections
 	bool shoreWaves = false;		// enables or disables shore waves
 	int dekorationDensity = 40;		// scale value for number of dekoration objects
 	bool allowRocks = true;			// enables or disables rock placement in rivers
 	float sizeScale = 6;			// has to be bigger than 0 and scales resolution of the terrain (bigger is more expensive for rendering and precomputation)
-	int wSGridSize = 128;			// wave simulation grid size (bigger is more expensive, recommended to use power of between 64 and 512)
+	int wSGridSize = 1024;			// wave simulation grid size (bigger is more expensive, recommended to use power of between 64 and 512)
 
 };
-
-
-
-
-
-
-
-
-
 
 
 settings gSettings =	selectedPreset == MIN ? settings { .ssr = false,										.shoreWaves = false,	.dekorationDensity = 0,								.sizeScale = 1,		.wSGridSize = 32	} :
