e663015866ec35a3614f660eaf8ff7097b19406d
Tom Uhlmann <Tom.Uhlmann.87@GMail.com>
2023-04-17 10:03:56 +0200

    fixed linking issue with new libigl version

--- SandboxMain.cpp
+++ SandboxMain.cpp
@@ -46,7 +46,7 @@ using namespace Eigen;
 //#define ActiveScene ExampleSkybox
 //#define ActiveScene ExampleTextRendering
 //#define ActiveScene ExampleShapesAndMaterials
-//#define ActiveScene ExampleLighting
+#define ActiveScene ExampleLighting
 //#define ActiveScene ExampleSceneGraph
 //#define ActiveScene ExampleSkeletalAnimation
 //#define ActiveScene ExampleMorphTargetAnimation
@@ -60,7 +60,7 @@ using namespace Eigen;
 //#define ActiveScene ForwardTestScene
 //#define ActiveScene PrimitiveFactoryTestScene
 //#define ActiveScene FrustumCullingTestScene
-#define ActiveScene StickFigureTestScene
+//#define ActiveScene StickFigureTestScene
 //#define ActiveScene AssetGLTFTestScene
 //#define ActiveScene ImGUITestScene
 
@@ -110,7 +110,7 @@ int main(int argc, char* argv[]) {
 
 		if (nullptr != pScene) delete pScene;
 		pScene = nullptr;
-		exportLibrary();
+		//exportLibrary();
 	}
 	catch (const CrossForgeException & e) {
 		SLogger::logException(e);
