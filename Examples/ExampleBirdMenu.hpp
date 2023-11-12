4acbfdb8e651c8c1132b9048d735ef50559808f9
ReyumDunk <maxmeyer811@web.de>
2023-11-12 15:55:35 +0100

    GUI elements added

--- Examples/ExampleBirdMenu.hpp
+++ Examples/ExampleBirdMenu.hpp
@@ -58,6 +58,22 @@ namespace CForge {
 				SLogger::log("OpenGL Error" + ErrorMsg, "PrimitiveFactoryTestScene", SLogger::LOGTYPE_ERROR);
 			}
 			*/
+			AssetIO::load("MyAssets/Menu/bird_sample.png", &ExampleImage);
+			AssetIO::load("MyAssets/Menu/bird_sample.png", &BirdImage);
+			AssetIO::load("MyAssets/Menu/bird_sample.png", &FlappyImage);
+
+			GLTexture2D MTexture;
+			MTexture.init(&ExampleImage);
+			ExampleTexture = MTexture.handle();
+			MTexture.clear();
+
+			MTexture.init(&BirdImage);
+			BirdTexture = MTexture.handle();
+			MTexture.clear();
+
+			MTexture.init(&FlappyImage);
+			FlappyTexture = MTexture.handle();
+			MTexture.clear();
 
 			m_ActiveTool = true;
 			m_DemoWindow = true;
@@ -72,6 +88,114 @@ namespace CForge {
 			ImGuiUtility::shutdownImGui();
 		}//clear
 
+		void TextCentered(std::string text) {
+			auto windowWidth = ImGui::GetContentRegionAvail().x;
+			auto textWidth = ImGui::CalcTextSize(text.c_str()).x;
+
+			ImGui::SetCursorPosX((windowWidth - textWidth) * 0.5f);
+			ImGui::Text(text.c_str());
+		}
+
+		void handleGUI(void) {
+			ImGuiUtility::newFrame();
+
+			//ImGuiUtility::newFrame();
+			//ImGui::ShowDemoWindow(&m_DemoWindow);
+			ImGui::Begin("My first Tool", &m_ActiveTool, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse);
+			ImGui::SetWindowPos(ImVec2(0, 0), ImGuiCond_Once);
+			ImGui::SetWindowSize(ImVec2(m_RenderWin.width(), m_RenderWin.height()));
+			ImVec2 image_size{ 300, 200 };
+			ImGui::SetCursorPosY(ImGui::GetCursorPosY() + 20);
+			TextCentered("Start any of the following examples");
+
+			ImGui::NewLine();
+			ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, 2.0f);
+			ImGui::SameLine((ImGui::GetContentRegionAvail().x / 3) - (image_size.x * 2 / 3));
+			ImGui::SetCursorPosY(ImGui::GetCursorPosY() + 20);
+			if (ImGui::ImageButton((void*)ExampleTexture, image_size)) {
+				startExample();
+				ImGui::PopStyleVar();
+				ImGui::End();
+				return;
+			}
+			ImGui::SameLine((ImGui::GetContentRegionAvail().x * 2 / 3) - (image_size.x / 3));
+			ImGui::SetCursorPosY(ImGui::GetCursorPosY() + 20);
+			if (ImGui::ImageButton((void*)BirdTexture, image_size)) {
+				startBirdVR();
+				ImGui::PopStyleVar();
+				ImGui::End();
+				return;
+			}
+
+			ImGui::NewLine();
+			string text = "Example";
+			auto textwidth = ImGui::CalcTextSize(text.c_str()).x;
+			ImGui::SameLine((ImGui::GetContentRegionAvail().x / 3) - (image_size.x / 6) - (textwidth / 2));
+			ImGui::Text("Example");
+
+			text = "Bird";
+			textwidth = ImGui::CalcTextSize(text.c_str()).x;
+			ImGui::SameLine((ImGui::GetContentRegionAvail().x * 2 / 3) + (image_size.x / 6) - (textwidth / 2));
+			ImGui::Text("Bird");
+
+			ImGui::NewLine();
+			ImGui::SameLine((ImGui::GetContentRegionAvail().x / 3) - (image_size.x * 2 / 3));
+			ImGui::SetCursorPosY(ImGui::GetCursorPosY() + 40);
+			if (ImGui::ImageButton((void*)FlappyTexture, image_size)) {
+				startFlappy();
+				ImGui::PopStyleVar();
+				ImGui::End();
+				return;
+			}
+
+			ImGui::NewLine();
+			text = "Flappy";
+			textwidth = ImGui::CalcTextSize(text.c_str()).x;
+			ImGui::SameLine((ImGui::GetContentRegionAvail().x / 3) - (image_size.x / 6) - (textwidth / 2));
+			ImGui::Text("Flappy");
+
+			ImGui::PopStyleVar();
+
+			ImGui::End();
+
+			return;
+		}
+
+		void startExample(void) {
+			ExampleMinimumGraphicsSetup* mScene = new ExampleMinimumGraphicsSetup();
+			m_RenderWin.closeWindow();
+			//ImGuiUtility::shutdownImGui();
+			mScene->init();
+			while (!mScene->renderWindow()->shutdown()) mScene->mainLoop();
+			if (nullptr != mScene) delete mScene;
+			mScene = nullptr;
+			init();
+			return;
+		}
+
+		void startBirdVR(void) {
+			ExampleBird* mScene = new ExampleBird();
+			m_RenderWin.closeWindow();
+			//ImGuiUtility::shutdownImGui();
+			mScene->init();
+			while (!mScene->renderWindow()->shutdown()) mScene->mainLoop();
+			if (nullptr != mScene) delete mScene;
+			mScene = nullptr;
+			init();
+			return;
+		}
+
+		void startFlappy(void) {
+			ExampleFlappyBird* mScene = new ExampleFlappyBird();
+			m_RenderWin.closeWindow();
+			//ImGuiUtility::shutdownImGui();
+			mScene->init();
+			while (!mScene->renderWindow()->shutdown()) mScene->mainLoop();
+			if (nullptr != mScene) delete mScene;
+			mScene = nullptr;
+			init();
+			return;
+		}
 
 		void mainLoop(void)override {
 			m_RenderWin.update();
@@ -89,62 +213,22 @@ namespace CForge {
 
 			m_RenderDev.activePass(RenderDevice::RENDERPASS_LIGHTING);
 			
-
-			
-			ImGuiUtility::newFrame();
-
-			//ImGuiUtility::newFrame();
-			ImGui::ShowDemoWindow(&m_DemoWindow);
-
-			ImGui::Begin("My first Tool", &m_ActiveTool, ImGuiWindowFlags_MenuBar);
-			ImGui::SetWindowPos(ImVec2(0, 0), ImGuiCond_Once);
-			if (ImGui::BeginMenuBar()) {
-				if (ImGui::BeginMenu("File")) {
-					if (ImGui::MenuItem("Open..", "Ctrl+O")) {}
-					if (ImGui::MenuItem("Close", "Ctrl+W")) { m_ActiveTool = false; printf("Close\n"); }
-					ImGui::EndMenu();
-				}
-				ImGui::EndMenuBar();
-			}
-			else {
-				SLogger::log("Problem creating ImGui window!");
-			}
-			ImGui::End();
-
+			handleGUI();
 			//ImGui::Render();
 			//ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 			ImGuiUtility::render();
-			
 
 			Keyboard* pKeyboard = m_RenderWin.keyboard();
 			if (pKeyboard->keyPressed(Keyboard::KEY_1, true)) {
-				ExampleMinimumGraphicsSetup* mScene = new ExampleMinimumGraphicsSetup();
-				m_RenderWin.closeWindow();
-				mScene->init();
-				while (!mScene->renderWindow()->shutdown()) mScene->mainLoop();
-				if (nullptr != mScene) delete mScene;
-				mScene = nullptr;
-				init();
+				startExample();
 			}
 
 			if (pKeyboard->keyPressed(Keyboard::KEY_2, true)) {
-				ExampleBird* mScene = new ExampleBird();
-				m_RenderWin.closeWindow();
-				mScene->init();
-				while (!mScene->renderWindow()->shutdown()) mScene->mainLoop();
-				if (nullptr != mScene) delete mScene;
-				mScene = nullptr;
-				init();
+				startBirdVR();
 			}
 
 			if (pKeyboard->keyPressed(Keyboard::KEY_3, true)) {
-				ExampleFlappyBird* mScene = new ExampleFlappyBird();
-				m_RenderWin.closeWindow();
-				mScene->init();
-				while (!mScene->renderWindow()->shutdown()) mScene->mainLoop();
-				if (nullptr != mScene) delete mScene;
-				mScene = nullptr;
-				init();
+				startFlappy();
 			}
 
 			m_RenderWin.swapBuffers();
@@ -163,6 +247,14 @@ namespace CForge {
 
 	protected:
 		SGNTransformation m_RootSGN;
+
+		T2DImage<uint8_t> ExampleImage;
+		T2DImage<uint8_t> BirdImage;
+		T2DImage<uint8_t> FlappyImage;
+
+		GLuint ExampleTexture;
+		uint32_t BirdTexture;
+		uint32_t FlappyTexture;
 
 		bool m_DemoWindow;
 		bool m_ActiveTool;
